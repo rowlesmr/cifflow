b7df76f592d68d9127f21fd70e72a54555f23a68
Matthew Rowles <rowlesmr@gmail.com>
2026-05-15 00:03:05 +0800

    Update release_patch.bat

--- release_patch.bat
+++ release_patch.bat
@@ -3,4 +3,6 @@ bump-my-version bump patch
 python -c "import re; v=re.search(r'(?m)^version = \"([\d.]+)\"', open('cifflow_core/Cargo.toml').read()).group(1); t=open('Cargo.lock').read(); t=re.sub(r'(name = \"cifflow_core\"\nversion = \")[\d.]+\"', r'\g<1>'+v+'\"', t); open('Cargo.lock','w',newline='\n').write(t)"
 git add Cargo.lock
 git commit --amend --no-edit
-git push && git push --tags
+for /f "delims=" %%v in ('python -c "import re; print(re.search(r\"(?m)^version = \\\"([\\d.]+)\\\"\", open('pyproject.toml').read()).group(1))"') do git tag -f v%%v HEAD
+git push && git push --tags --force-with-lease
+pause
